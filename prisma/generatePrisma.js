e2d261377f54f637e4c684e8290db9f9ce8890e4
WilliamBPFR <1104228@est.intec.edu.do>
2024-01-19 19:13:27 -0400

    scaasca

--- prisma/generatePrisma.js
+++ prisma/generatePrisma.js
@@ -1,6 +1,24 @@
+// generateSchema.js
 
+const fs = require('fs');
+const path = require('path');
+const config = require('./../config.json')
+
+
+// Lee la configuración desde config.json
+// const configFile = path.join(__dirname, 'config.json');
+// const configFileContent = fs.readFileSync(configFile, 'utf-8');
+// const configJson = JSON.parse(configFileContent);
+
+// Construye el contenido del archivo schema.prisma
+const schemaContent = `
 generator client {
   provider = "prisma-client-js"
+}
+
+datasource db {
+  provider = "mysql"
+  url      = "${config.DATABASE_URL || 'mada'}"
 }
 
 model Usuario {
@@ -24,3 +42,10 @@ model Usuario {
     Usuario            Usuario  @relation(fields: [id_usuario], references: [id_usuario])
   }
   
+`;
+
+// Escribe el contenido en el archivo schema.prisma
+const schemaFile = path.join(__dirname, 'schema.prisma');
+fs.writeFileSync(schemaFile, schemaContent);
+
+console.log('Generated schema.prisma with databaseUrl:', config.DATABASE_URL || 'fallback');
